e0ec444de44d3a45b87b2e82c04e1f3dee67c9c7
marlonchiu <Knight1898@outlook.com>
2019-09-10 10:49:46 +0800

    初始化构建项目

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue_shop_webapp
+
+一个基于vue + Koa2的移动端的电商系统
---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    '@vue/app'
+  ],
+  // vant 按需引入
+  plugins: [
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
+  ]
+}
