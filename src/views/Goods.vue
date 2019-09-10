638ef6a534390c1d8217ce9eefd270b1d06595a5
marlonchiu <Knight1898@outlook.com>
2019-09-10 11:45:45 +0800

    开发准备工作

---
+++ src/views/Goods.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Goods',
+    data() {
+      return {
+        msg: 'Goods Page'
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/ShoppingMall.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <van-row>
+      <van-col span="8">span: 8</van-col>
+      <van-col span="8">span: 8</van-col>
+      <van-col span="8">span: 8</van-col>
+    </van-row>
+    ShoppingMall Pages
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ShoppingMall',
+    data () {
+      return {
+        msg: 'ShoppingMall'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
