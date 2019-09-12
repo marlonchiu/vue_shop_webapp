d91935321755e4023ab32a8755893a21bc6af3ff
marlonchiu <Knight1898@outlook.com>
2019-09-12 10:41:49 +0800

    存储数据 加密加盐bcrypt

--- service/database/schema/user.js
+++ service/database/schema/user.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose') // 引入Mongoose
 const Schema = mongoose.Schema // 声明Schema
 let ObjectId = Schema.Types.ObjectId // 声明Object类型
+const bcrypt = require('bcrypt') // 数据加密加盐处理
+const SALT_WORK_FACTOR = 10
 
 // 创建用户Schema
 const userSchema = new Schema({
@@ -11,6 +13,19 @@ const userSchema = new Schema({
   password: String,
   createAt: { type: Date, default: Date.now() },
   lastLoginAt: { type: Date, default: Date.now() }
+})
+// 每次存储数据时都要执行
+userSchema.pre('save', function (next) {
+  // let user = this  this就是user
+  console.log(this)
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err)
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err)
+      this.password = hash
+      next()
+    })
+  })
 })
 
 // 发布模型
