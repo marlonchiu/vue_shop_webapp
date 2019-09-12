158007724e15c633fd9d68b15cbe003bea1bd1f0
marlonchiu <Knight1898@outlook.com>
2019-09-12 10:59:24 +0800

    前端注册页面实现

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { Button, Row, Col, Swipe, SwipeItem, Lazyload, List } from 'vant'
-Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(List)
+import { Button, Row, Col, Swipe, SwipeItem, Lazyload, List, Field, NavBar } from 'vant'
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(List).use(Field).use(NavBar)
 
 // 只有在生产环境下使 用mock
 // if (process.env.NODE_ENV !== 'production') require('./mock')
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 const ShoppingMall = () => import('@/views/ShoppingMall.vue')
+const Register = () => import('@/views/Register.vue')
 
 const routes = [
   {
     path: '/',
     name: 'ShoppingMall',
     component: ShoppingMall
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   }
 ]
 
